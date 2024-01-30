1ffbf5bb9f48f2b031cf6fbb81998119f6739764
angusgee <maltawebdev@gmail.com>
2024-01-30 19:29:14 +0100

    add comment for degugging

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ class Window:
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
         self.canvas.pack()
         self.running = True
+        self.root.protocol('WM_DELETE_WINDOW', self.close)
+        self.canvas.pack()
+        
         
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
         
-
+    def wait_for_close(self):
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+        
 def main():        
+    print('starting now....')
     win = Window(420, 420)
+    win.wait_for_close()
     
 if __name__ == '__main__':
     main()
